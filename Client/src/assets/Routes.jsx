f5fb54f2d90efc6c3dca5bd6b968c2149bbd1780
Matthew Farish <97074732+matt-farish@users.noreply.github.com>
2023-05-20 14:29:45 +1000

    Continued work on MERN chat app
    Added a styled chat page, online visibility status

--- Client/src/assets/Routes.jsx
+++ Client/src/assets/Routes.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "../RegisterAndLoginForm";
 import { UserContext } from "../UserContext";
+import Chat from "../Chat";
 
 export default function Routes() {
-    const {username, id} = useContext(UserContext);
+  const { username, id } = useContext(UserContext);
 
-    if (username) {
-        return 'Logged in!' + username;
-    }
-    return (
-        <RegisterAndLoginForm />
-    )
-}  
+  if (username) {
+    return <Chat />
+  }
+  return <RegisterAndLoginForm />;
+}
